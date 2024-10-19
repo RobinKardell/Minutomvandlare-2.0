d77d68134d352722bddf4aa1d3be2bc51936f32a
Robin Kardell <carden.rk@gmail.com>
2024-10-19 23:07:43 +0200

    Gjorde en väldig enkel minutomvandlare

--- Minutomvandlare 2.0/Form1.Designer.cs
+++ Minutomvandlare 2.0/Form1.Designer.cs
@@ -28,18 +28,82 @@
         /// </summary>
         private void InitializeComponent()
         {
+            label1 = new Label();
+            pageSetupDialog1 = new PageSetupDialog();
+            txtMinutes = new TextBox();
+            btnConvert = new Button();
+            label2 = new Label();
+            lblResult = new Label();
             SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 8);
+            label1.Name = "label1";
+            label1.Size = new Size(184, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Write in minute you will converts ";
+            // 
+            // txtMinutes
+            // 
+            txtMinutes.Location = new Point(12, 26);
+            txtMinutes.Name = "txtMinutes";
+            txtMinutes.Size = new Size(172, 23);
+            txtMinutes.TabIndex = 1;
+            // 
+            // btnConvert
+            // 
+            btnConvert.Location = new Point(12, 55);
+            btnConvert.Name = "btnConvert";
+            btnConvert.RightToLeft = RightToLeft.Yes;
+            btnConvert.Size = new Size(172, 23);
+            btnConvert.TabIndex = 2;
+            btnConvert.Text = "Calculate";
+            btnConvert.UseVisualStyleBackColor = true;
+            btnConvert.Click += btnConvert_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(233, 8);
+            label2.Name = "label2";
+            label2.Size = new Size(39, 15);
+            label2.TabIndex = 3;
+            label2.Text = "Result";
+            // 
+            // lblResult
+            // 
+            lblResult.AutoSize = true;
+            lblResult.Location = new Point(233, 29);
+            lblResult.Name = "lblResult";
+            lblResult.Size = new Size(34, 15);
+            lblResult.TabIndex = 4;
+            lblResult.Text = "none";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(446, 370);
+            ClientSize = new Size(446, 192);
+            Controls.Add(lblResult);
+            Controls.Add(label2);
+            Controls.Add(btnConvert);
+            Controls.Add(txtMinutes);
+            Controls.Add(label1);
             Name = "Form1";
-            Text = "Form1";
+            Text = "Minutomvandlare 2.0";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private PageSetupDialog pageSetupDialog1;
+        private TextBox txtMinutes;
+        private Button btnConvert;
+        private Label label2;
+        private Label lblResult;
     }
 }
--- Minutomvandlare 2.0/Form1.cs
+++ Minutomvandlare 2.0/Form1.cs
@@ -6,5 +6,65 @@ namespace Minutomvandlare_2._0
         {
             InitializeComponent();
         }
+
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            int minutes;
+            if(int.TryParse(txtMinutes.Text, out minutes))
+            {
+                int years = minutes / (60 * 24 * 365);
+                minutes %= 60 * 24 * 365;
+                int months = minutes / (60 * 24 * 30);
+                minutes %= 60 * 24 * 30;
+                int days = minutes / (60 * 24);
+                minutes %= 60 * 24;
+                int hours = minutes / 60;
+                int remainingMinutes = minutes % 60;
+
+                lblResult.Text = "";
+
+                // Skriv ut år
+                if (years > 0)
+                {
+                    lblResult.Text += $"{years} " + (years == 1 ? "år" : "år") + "\n";
+                }
+
+                // Skriv ut månader, grått om 0
+                if (months > 0)
+                {
+                    lblResult.Text += $"{months} " + (months == 1 ? "månad" : "månader") + "\n";
+                }
+                else if (years > 0)
+                {
+                    lblResult.Text += "0 månader\n";
+                    lblResult.ForeColor = Color.Gray;
+                }
+
+                // Skriv ut dagar
+                if (days > 0)
+                {
+                    lblResult.Text += $"{days} " + (days == 1 ? "dag" : "dagar") + "\n";
+                }
+
+                // Skriv ut timmar och minuter om relevant
+                if (hours > 0)
+                {
+                    lblResult.Text += $"{hours} " + (hours == 1 ? "timme" : "timmar");
+
+                    if (remainingMinutes > 0)
+                    {
+                        lblResult.Text += $" och {remainingMinutes} " + (remainingMinutes == 1 ? "minut" : "minuter");
+                    }
+                }
+                else if (remainingMinutes > 0) // Om inga timmar, men minuter finns
+                {
+                    lblResult.Text += $"{remainingMinutes} " + (remainingMinutes == 1 ? "minut" : "minuter");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Ange ett giltigt antal minuter.");
+            }
+        }
     }
 }
